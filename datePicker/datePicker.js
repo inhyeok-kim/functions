ca8d1771d1b40cf93885efb7e1083a7f270d626f
inhyeok-kim <inhyeok.kim@icloud.com>
2022-03-11 09:45:49 +0900

    1. 달력 완성

--- datePicker/datePicker.js
+++ datePicker/datePicker.js
@@ -7,6 +7,11 @@ window.addEventListener('load',function(){
     
 });
 
+let today = new Date();
+let currnetY = today.getFullYear();
+let currnetD = today.getDate();
+let currnetM = today.getMonth();
+
 function openDatePicker(e){
     console.log(e);
 }
@@ -20,31 +25,104 @@ function createDatePickerContainer(){
     dpc.style.height = '400px';
     dpc.style.boxSizing = 'border-box';
     dpc.style.border = '1px solid black';
-
+    dpc.style.display = 'flex';
+    dpc.style.flexWrap = 'wrap';
     let today = new Date();
     const start = today.setDate(0);
 
+    createCalendar();
 }
 
 function createCalendar(){
-    const today = new Date();
-    const currnetY = today.getFullYear();
-    const currnetD = today.getDate();
-    const currnetM = today.getMonth();
+    const dpc = document.getElementById('datePickerContainer');
+    dpc.innerHTML = '';
+
     const startDate = new Date(currnetY, currnetM, 1);
     const endDate = new Date(currnetY, currnetM+1, 0);
     const startDay = startDate.getDay();
-    
-    for(let i = 0 ; )
-}
+    const endDay = endDate.getDay();
 
-createCalendar();
+    const rowCnt = (startDay + endDate.getDate() + (6-endDay)) / 7;
+
+
+    const ctrl = document.createElement('div');
+    ctrl.style.width = '100%';
+    ctrl.style.height = '10%';
+    dpc.append(ctrl);
+
+    const prevM = document.createElement('button');
+    prevM.innerText = '이전달';
+    prevM.onclick = prevMonth;
+    ctrl.append(prevM);
+
+    const yearMonth = document.createElement('span');
+    yearMonth.innerText = `${currnetY}년 ${currnetM+1}월`;
+    ctrl.append(yearMonth);
+
+    const goTodayBtn = document.createElement('a');
+    goTodayBtn.innerText = '오늘';
+    goTodayBtn.href = 'javascript:goToday()';
+    ctrl.append(goTodayBtn);
+
+    const nextM = document.createElement('button');
+    nextM.innerText = '다음달';
+    nextM.onclick = nextMonth;
+    ctrl.append(nextM);
+
+    const calc = document.createElement('div');
+    calc.style.width = '100%';
+    calc.style.height = '90%';
+    calc.style.display = 'flex';
+    calc.style.flexWrap = 'wrap';
+    calc.id = "datePickerCalendarContainer";
+    dpc.append(calc);
+
+    for(let i = 0 ; i < 7; i++){
+        const div = document.createElement('div');
+        div.className = 'day';
+        div.style.width = 'calc(100% / 7)';
+        div.style.height = 'calc(10%)';
+        div.innerText = transDayToKor(i);
+        div.style.boxSizing = 'border-box';
+        div.style.border = '1px solid black';
+        div.style.textAlign = 'center';
+        calc.append(div);
+    }
+    for(let i = 0 ; i < startDay; i++){
+        const div = document.createElement('div');
+        div.className = 'blank-date';
+        div.style.width = 'calc(100% / 7)';
+        div.style.height = 'calc(90% / '+rowCnt+')';
+        div.style.boxSizing = 'border-box';
+        div.style.border = '1px solid black';
+        calc.append(div);
+    }
+    for(let i = startDate.getDate() ; i <= endDate.getDate(); i++){
+        const div = document.createElement('div');
+        div.className = 'date';
+        div.innerText = i;
+        div.style.width = 'calc(100% / 7)';
+        div.style.height = 'calc(90% / '+rowCnt+')';
+        div.style.boxSizing = 'border-box';
+        div.style.border = '1px solid black';
+        calc.append(div);
+    }
+    for(let i = 0 ; i < 6 - endDay; i++){
+        const div = document.createElement('div');
+        div.className = 'blank-date';
+        div.style.width = 'calc(100% / 7)';
+        div.style.height = 'calc(90% / '+rowCnt+')';
+        div.style.boxSizing = 'border-box';
+        div.style.border = '1px solid black';
+        calc.append(div);
+    }
+}
 
 function transDayToKor(day){
     switch (day) {
         case 0:
             return '일';
-        case 1:
+            case 1:
             return '월';
         case 2:
             return '화';
@@ -57,4 +135,29 @@ function transDayToKor(day){
         case 6:
             return '토';
     }
+}
+
+function prevMonth(){
+    const nextDate = new Date(currnetY, currnetM-1, currnetD);
+    setCurrentDate(nextDate.getFullYear(), nextDate.getMonth(), nextDate.getDate());
+    createCalendar();
+}
+
+function nextMonth(){
+    const nextDate = new Date(currnetY, currnetM+1, currnetD);
+    setCurrentDate(nextDate.getFullYear(), nextDate.getMonth(), nextDate.getDate());
+    createCalendar();
+}
+
+function goToday(){
+    const nextDate = new Date();
+    setCurrentDate(nextDate.getFullYear(), nextDate.getMonth(), nextDate.getDate());
+    createCalendar();
+}
+
+function setCurrentDate(year, month, date){
+    today = new Date();
+    currnetD = date;
+    currnetM = month;
+    currnetY = year;
 }
